39f8d7f6c44a0b2e27ad1f5e630b7c092e0f580f
7azemIbrahim <121644999+7azemIbrahim@users.noreply.github.com>
2022-12-30 01:06:24 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prime
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int S = int.Parse(Console.ReadLine());
+            int E = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int i = S; i <= E; i++)
+            {
+                for (int x = 1; x <= i; x++)
+                {
+                    if (i % x == 0)
+                        sum++;
+                }
+                if (sum == 2)
+                {
+                    Console.WriteLine(i);
+                    sum = 0;
+                }
+                else
+                {
+                    sum = 0;
+                    }
+            }
+        }
+    }
+}
